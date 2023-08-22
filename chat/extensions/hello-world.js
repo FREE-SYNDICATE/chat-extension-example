af3c26679defa1e4be73568a86b9e29b924d13eb
Max Countryman <maxc@me.com>
2023-08-22 09:31:00 -0700

    debug

--- chat/extensions/hello-world.js
+++ chat/extensions/hello-world.js
@@ -1,1 +1,107 @@
-console.log("Hello, from JS!");
+import { addRxPlugin, createRxDatabase, lastOfArray } from "rxdb";
+
+import { RxDBDevModePlugin } from "rxdb/plugins/dev-mode";
+import { replicateRxCollection } from "rxdb/plugins/replication";
+import { getRxStorageMemory } from "rxdb/plugins/storage-memory";
+
+addRxPlugin(RxDBDevModePlugin);
+
+const db = await createRxDatabase({
+  name: "database", // TODO: Does this matter?
+  storage: getRxStorageMemory(),
+});
+
+const state = { replications: {}, canonicalDocumentChanges: {} };
+
+function getReplicationStateKey(collectionName) {
+  return `${collectionName}ReplicationState`;
+}
+
+function getCanonicalDocumentChangesKey(collectionName) {
+  return `${collectionName}CanonicalDocumentChanges`;
+}
+
+async function createCollectionsFromCanonical(collections) {
+  await db.addCollections(collections);
+
+  for (const collection of db.collections) {
+    const replicationState = await createReplicationState(collection);
+    const replicationStateKey = getReplicationStateKey(collection.name);
+    state.replications[replicationStateKey].property = replicationState;
+  }
+}
+
+async function createReplicationState(collection) {
+  const { name: collectionName } = collection;
+
+  const replicationState = replicateRxCollection({
+    collection,
+    replicationIdentifier: `${collectionName}-replication`,
+    live: true,
+    retryTime: 5 * 1000,
+    waitForLeadership: true,
+    autoStart: true,
+
+    deletedField: "deleted", // TODO.
+
+    push: {
+      async handler(docs) {
+        console.log("Called handler with: ", docs);
+
+        // TODO:
+        // window.postMessage(JSON.stringify(docs), "*");
+
+        return {};
+      },
+
+      batchSize: 5,
+      modifier: (d) => d,
+    },
+
+    pull: {
+      async handler(lastCheckpoint, batchSize) {
+        console.log("Called pull handler with: ", lastCheckpoint, batchSize);
+
+        const canonicalDocumentChangesKey =
+          getCanonicalDocumentChangesKey(collectionName);
+        const documents =
+          state.canonicalDocumentChanges[canonicalDocumentChangesKey]; // TODO: Clear on processing? Batch size?
+        const checkpoint =
+          documents.length === 0
+            ? lastCheckpoint
+            : {
+                id: lastOfArray(documents).id,
+                updatedAt: lastOfArray(documents).updatedAt,
+              };
+
+        window[`${collectionName}LastCheckpoint`] = checkpoint;
+
+        return {
+          documents,
+          checkpoint,
+        };
+      },
+
+      batchSize: 10,
+      modifier: (d) => d,
+    },
+  });
+
+  return replicationState;
+}
+
+function syncDocsFromCanonical(collectionName, changedDocs) {
+  const replicationStateKey = getReplicationStateKey(collectionName);
+  const replicationState = state.replications[replicationStateKey];
+
+  const canonicalDocumentChangesKey =
+    getCanonicalDocumentChangesKey(collectionName);
+
+  state.canonicalDocumentChanges[canonicalDocumentChangesKey].property =
+    changedDocs;
+
+  replicationState.reSync();
+}
+
+window.createCollectionsFromCanonical = createCollectionsFromCanonical;
+window.syncDocsFromCanonical = syncDocsFromCanonical;
